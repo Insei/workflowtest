d1b9efa7b72469584bc3cc1fa2de7146e6cfe9cf
Dmitriy <goodmobiledevices@gmail.com>
2021-05-18 10:41:48 +0300

    Update wait-eth0.sh

--- shell-scripts/packet/wait-eth0.sh
+++ shell-scripts/packet/wait-eth0.sh
@@ -16,7 +16,11 @@ __EOT__
 `
 
 function packet-cli() {
-    "$HOME"/go/bin/packet-cli $@
+   if [ -e $HOME/go/bin/packet-cli ]; then
+     "$HOME"/go/bin/packet-cli $@
+   else
+      $GOPATH/bin/packet-cli $@
+   fi
 }
 
 function packet_cli_get_ip() {
